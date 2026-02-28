a223ba3450581038bb56a7423619829c96e4a189
Darryl <darryl@flyingmanproductions.com>
2026-02-28 10:33:38 -0600

    Add website .gitignore, remove committed build artifacts

--- website/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z11zXGe from "../../assets/icon.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/icon.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z11zXGe]]);
-		
